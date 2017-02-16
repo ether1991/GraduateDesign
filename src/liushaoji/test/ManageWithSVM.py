e2282e4cc2e1905a1f94bbdca0444092f57d0a1f
ether1991 <ether1991@hotmail.com>
2017-02-16 21:44:14 +0800

    2017.02.16

--- src/liushaoji/test/ManageWithSVM.py
+++ src/liushaoji/test/ManageWithSVM.py
@@ -1,24 +1,27 @@
 from sklearn import svm
 import pandas
+from sklearn.model_selection import train_test_split
 from sklearn.cross_validation import cross_val_score
 import numpy as np
 inputFile = pandas.read_csv('/Users/liushaoji/PycharmProjects/GraduateDesign/file/Day01_format.csv');
 inputFile.dropna(inplace=True)  # drop the NaN records
 inputData = inputFile.values
 # data feature
-X = inputData[:, 1:8]
+data = inputData[:, 1:8]
 # label
-Y = inputData[:, 8]
+label = inputData[:, 8]
 
-clf = svm.SVC(kernel='sigmoid')  # class  It must be one of 'linear', 'poly', 'rbf', 'sigmoid', 'precomputed'
-clf.fit(X, Y)  # training the svc model
+X_train, X_test, Y_train, Y_test = train_test_split(data, label, test_size=0.3, random_state=0)
+
+clf = svm.SVC(kernel='linear', C=1) # class  It must be one of 'linear', 'poly', 'rbf', 'sigmoid', 'precomputed'
+clf.fit(X_train, Y_train)  # training the svc model
 #
 # result = clf.predict([7.7,2.8,6.7,2.0])  # predict the target of testing samples
 # print result  # target
 # acc = clf.score(X, Y)
 # print acc
 
-scores = cross_val_score(clf, X, Y)
+scores = cross_val_score(clf, X_test, Y_test)
 print scores
 #
 # print clf.support_vectors_  # support vectors
